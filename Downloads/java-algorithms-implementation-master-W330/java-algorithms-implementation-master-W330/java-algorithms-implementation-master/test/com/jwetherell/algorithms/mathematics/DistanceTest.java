886c8ad73a5bca958314aeea65b68183173e6e3e
ronakp12 <129319035+ronakp12@users.noreply.github.com>
2024-11-20 17:58:31 -0500

    Quiz 3 done

---
+++ Downloads/java-algorithms-implementation-master-W330/java-algorithms-implementation-master-W330/java-algorithms-implementation-master/test/com/jwetherell/algorithms/mathematics/DistanceTest.java
@@ -0,0 +1,20 @@
+package com.jwetherell.algorithms.mathematics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DistanceTest {
+
+    @Test
+    void chebyshevDistance() {
+    }
+
+    @Test
+    void squaredDistance() {
+    }
+
+    @Test
+    void euclideanDistance() {
+    }
+}
---
+++ Downloads/java-algorithms-implementation-master-W330/java-algorithms-implementation-master-W330/java-algorithms-implementation-master/test/com/jwetherell/algorithms/mathematics/DivisionTest.java
@@ -0,0 +1,31 @@
+package com.jwetherell.algorithms.mathematics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DivisionTest {
+
+    @Test
+    void division() {
+    }
+
+    @Test
+    void divisionUsingLoop() {
+    }
+
+    @Test
+    void divisionUsingRecursion() {
+    }
+
+    @Test
+    void divisionUsingMultiplication() {
+    }
+
+    @Test
+    void divisionUsingShift() {
+    }
+
+    @Test
+    void divisionUsingLogs() {
+    }
+}
